d7fc31fa5f1dd7b58d718548be13604b2f98d0bb
Shannon Prescott <shan.prescott@outlook.com>
2018-01-18 19:59:49 +0000

    Done.

--- array.js
+++ array.js
@@ -1,17 +1,19 @@
 function addElementToBeginningOfArray(array, element) {
- // add the element to the beginning of the array without mutating the array
+  return [element,...array]
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  return array.unshift[element]
 }
 
 function addElementToEndOfArray(array, element) {
-
+  return [...array, element]
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+  array.push[element]
+  return array
 }
 
 function accessElementInArray(array, index) {
