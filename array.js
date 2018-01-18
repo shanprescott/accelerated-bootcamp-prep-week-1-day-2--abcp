bf3beef95e78fcd299cf690a335b9e10d744fa7e
Shannon Prescott <shan.prescott@outlook.com>
2018-01-18 20:16:16 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -17,7 +17,7 @@ function destructivelyAddElementToEndOfArray(array, element) {
 }
 
 function accessElementInArray(array, index) {
-
+  return array[index]
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
